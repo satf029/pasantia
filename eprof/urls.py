51ce9287448065fae9f6713c25749279774bee00
Samuel Torres <torressamuel.ferna@gmail.com>
2024-12-03 12:13:22 -0300

    actualizaciones

--- eprof/urls.py
+++ eprof/urls.py
@@ -2,8 +2,6 @@
 from django.urls import path
 from eprof import views
 
-app_name = 'eprof'
-
 urlpatterns = [
     path('',views.login, name='login'),
     path('callback',views.callback, name='callback'),
